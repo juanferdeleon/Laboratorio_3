9f6462a8d0e7680df778609f6005096731663095
Juan De Leon <dele17822@uvg.edu.gt>
2019-02-05 21:04:28 -0600

    Gnome and Merge Sorts

--- src/Gnome.java
+++ src/Gnome.java
@@ -1,2 +1,26 @@
+
 public class Gnome {
+
+    public int[] gnomeSort(int[] infoArray){
+
+        for (int i = 1; i <= infoArray.length;){
+
+            if (infoArray[i - 1] <= infoArray[i])
+                ++i;
+
+            else {
+
+                int tempValue = infoArray[i];
+                infoArray[i] = infoArray[i - 1];
+                infoArray[i - 1] = tempValue;
+                --i;
+
+                if (i == 0)
+                    i = 1;
+
+            }
+        }
+        return infoArray;
+    }
+
 }
