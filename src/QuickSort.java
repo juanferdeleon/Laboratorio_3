babbcdc11984da72e9777a89f38a69ce385d143b
amado-developer <amado_developer@hotmail.com>
2019-02-06 08:10:08 -0600

    Starting quicksort

---
+++ src/QuickSort.java
@@ -0,0 +1,14 @@
+public class QuickSort {
+    private int pivot;
+    private int left;
+    private int right;
+    private int[] number;
+
+    public QuickSort(int[] number){
+        this.number = number;
+    }
+
+    public int[] Sort(){
+        
+    }
+}
