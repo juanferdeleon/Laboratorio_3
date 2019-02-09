85679b9c2c9bddbf5451d6137bbe2b5af0faabcf
JavierRamirezCospin <Ram18099@uvg.edu.gt>
2019-02-08 22:07:21 -0600

    main

--- src/Main.java
+++ src/Main.java
@@ -5,24 +5,19 @@ import java.util.Random;
 public class Main {
     public static void main(String[] args){
 
-        Numbers[] numbers = new Numbers[4];
-        numbers[0] = new Numbers(2000);
-        numbers[1] = new Numbers(200);
-        numbers[2] = new Numbers(1);
-        numbers[3] = new Numbers(1);
+        WriteFile("./Numbers.txt");
+        Comparable[] numbers = ReadFile("./Numbers.txt");
 
-        Radix.bucketPass(numbers, 5);
 
-        for (int i = 0; i < 4; i++) {
-            System.out.println(numbers[i]);
+        Merge.mergeSort(numbers, 3000);
+        for (int i = 0; i <  numbers.length; i++) {
+            System.out.println("{ " +numbers[i] + " }" );
         }
-
     }
 
-    public static Numbers[] ReadFile(String fileName){
+    public static Comparable[] ReadFile(String fileName) {
 
-        ArrayList infoArray =  new ArrayList();
-        Numbers[] numbers;
+        Comparable[] numbers = new Numbers[3000];
         int i = 0;
 
         try {
@@ -31,23 +26,21 @@ public class Main {
 
             String line = bufferedReader.readLine();
 
-            while (line != null){
-                infoArray.add(i, Integer.parseInt((line)));
+            while (line != null) {
                 line = bufferedReader.readLine();
+                int number = Integer.parseInt((line));
+                numbers[i] = new Numbers(number);
                 i++;
             }
         } catch (Exception var10) {
             System.out.println(var10);
         }
 
-        numbers = new Numbers[i];
 
-        for (int j = 0; j <= i; j++){
-            numbers[i] = (Numbers)infoArray.get(i);
-        }
+            return numbers;
 
-        return numbers;
     }
+
 
     public static void WriteFile(String fileName){
         try{
@@ -59,7 +52,7 @@ public class Main {
             Random generator = new Random();
 
             for (int i = 0; i <= 3000; i++) {
-                int num = generator.nextInt(3000); //generate a random number
+                int num = generator.nextInt(3001); //generate a random number
                 bw.write("" + num ); //write the number to the file
                 bw.newLine();
             }
